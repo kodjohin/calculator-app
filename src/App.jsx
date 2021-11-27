8c83f9f56362b104ac473f7e1dfd633ab3cb3bbe
kodjohin <kodjo.ohin@gmail.com>
2021-11-27 20:01:08 +0100

    set app, first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./App.css"
+import styled from 'styled-components'
+
+const StyledApp = styled.div`
+    display: flex;
+`
+
+const App = () => {
+    return (
+        <StyledApp>
+            
+        </StyledApp>
+    )
+}
+
+export default App
